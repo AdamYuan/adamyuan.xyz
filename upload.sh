603bbecc62669fbacedf69b368f819d7d527a35d
AdamYuan <y13916619121@126.com>
2018-08-31 17:26:01 +0800

    update Fri Aug 31 17:26:01 CST 2018

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@ hugo && \
 git add * && \
 git commit -m "update $(date)" && \
 git push origin master && \
-ssh root@adamyuan.xyz './update_site.sh'
+ssh root@adamyuan.xyz 'cd /var/www/adamyuan.xyz/ && git pull origin master'
