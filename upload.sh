3aa19af0035ef881cce8c325e7cf67f0f20e220f
AdamYuan <y13916619121@126.com>
2018-09-09 11:39:00 +0800

    update Sun Sep  9 11:39:00 CST 2018

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -rf ~/HugoBlog/public/ && \
-hugo && \
-git add * && \
-git commit -m "update $(date)" && \
-git push origin master && \
+rm -rf ~/HugoBlog/public/
+hugo
+git add *
+git commit -m "update $(date)"
+git push origin master
 ssh root@adamyuan.xyz 'cd /var/www/adamyuan.xyz/ && git pull origin master'
