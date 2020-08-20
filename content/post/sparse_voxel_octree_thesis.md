1c0acafa2f6eb3e481a38efc4fbcff8b34a90944
AdamYuan <y13916619121@126.com>
2020-08-20 12:30:59 +0800

    update 2020年 08月 20日 星期四 12:30:59 CST

--- content/post/sparse_voxel_octree_thesis.md
+++
@@ -1,9 +0,0 @@
----
-title: "基于 GPU 的稀疏体素八叉树构建以及路径追踪的设计与实现"
-date: 2020-02-24T11:28:19+08:00
-draft: true
-tags: [ "Sparse Voxel Octree", "Path Tracing" ]
-categories: [ "Computer Graphics" ]
----
-
-
--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf ~/HugoBlog/public/
-hugo
-git add *
-git rm config.toml --cached
-git commit -m "update $(date)"
-git push origin master
-ssh root@adamyuan.xyz 'cd /var/www/adamyuan.xyz/ && git pull origin master'
